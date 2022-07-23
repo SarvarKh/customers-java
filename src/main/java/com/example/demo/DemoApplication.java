5efbfac8d88517c8df9d1ad3a3f18fd876a05040
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-07-23 10:44:57 +0500

    Create simople API

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,39 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+// Create RESTfull end point
+@SpringBootApplication // Kick-offs the app
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	@GetMapping // Http method to associate this method;
+	Customer getCustomer() {
+		return new Customer(1L, "James Bond");
+	}
 
+	// This is simple RESTfull API
+	class Customer {
+		private final Long id;
+		private final String name;
+
+		public Customer(Long id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+	}
 }
