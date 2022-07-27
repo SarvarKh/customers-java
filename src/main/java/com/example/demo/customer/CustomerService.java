8204e51e50c3ced3102865fdbff0defcda34d6bb
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-07-27 17:03:46 +0500

    Complete logging:

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo.customer;
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import java.util.List;
 
 @Service
 public class CustomerService { //now this is bean that we can inject in multiple places
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -19,13 +22,20 @@ public class CustomerService { //now this is bean that we can inject in multiple
     }
 
     List<Customer> getCustomers() {
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id) {
         return customerRepository
                 .findById(id)
-                .orElseThrow(() -> new NotFoundException(
-                        "customer with id " + id + " not found"));
+                .orElseThrow(
+                        () -> {
+                            NotFoundException notFoundException = new NotFoundException(
+                                    "customer with id " + id + " not found");
+                            LOGGER.error("error in getting customer {}", id, notFoundException);
+                            return notFoundException;
+                        });
+
     }
 }
